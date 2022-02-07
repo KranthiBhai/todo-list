754ec1f9e7d6b74f5588ffbf262e51a6f6fd4772
Kranthi <kk232243@gmail.com>
2022-02-07 22:19:03 +0530

    created todo component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import { TodoListItem } from './TodoListItem';
 
+
+const todos: Todo[] = [
+  {
+    text:"Walk the dog",
+    complete: false,
+  },
+  {
+    text: "Write app",
+    complete: true,
+  },
+];
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+     <ul>
+       <TodoListItem todo={todos[0]} />
+       <TodoListItem todo={todos[1]} />
+     </ul>
     </div>
   );
 }
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+// What's nice about  a *.d.ts file is our compiler will recognize the types in there as global to the project
+// and we won't need to explicitly import or export them.
+
+interface Todo {
+    text : string,
+    complete: boolean,
+}
